de4e826e876c5eb8ffdc1f919c184d53bc814d74
Aviv Issachar <avivi@misterbit.co.il>
2023-02-01 17:10:08 +0200

    build

---
+++ public/frontend/precache-manifest.f0a96eb03363d4ffde17f30a09824db7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213e507cbc39223c87c8",
+    "url": "/css/app.7831ec16.css"
+  },
+  {
+    "revision": "3682c9231d5a7236c441cb6221f5dd5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "213e507cbc39223c87c8",
+    "url": "/js/app.347442e0.js"
+  },
+  {
+    "revision": "e360ec28cb6d223fffaa",
+    "url": "/js/chunk-vendors.d693e067.js"
+  },
+  {
+    "revision": "82db4b82a5a74e835be5e8cb69f27350",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
